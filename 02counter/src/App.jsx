9938e8d19ddafba33f356b32cac944e373d15004
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-18 19:15:59 +0530

    use state hook and setCounter method

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+let [counter , setCounter] = useState(10)
+const addValue =  () =>{
+    // counter = counter +1;
+    console.log("value of counter is: " , counter)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    console.log("value of counter is: " , counter)
+  
+
+}
+
+const removeValue = () => {
+  if(counter>=0){
+
+    setCounter(counter)
+    counter= counter - 1;
+    console.log("value of counter is counter" , counter);
+  }
+}
+
+  return (
+    <>
+      
+        <h1>Counter </h1>
+        <h3>Counter : {counter} </h3>
+        <p>more counter : {counter} </p>
+
+        <button onClick={addValue}>increase value : {counter}</button>
+        <br />
+        <br />
+        <button onClick={removeValue}> decrease value: {counter}</button>
+
+
+    </>
+  )
+}
+
+export default App
