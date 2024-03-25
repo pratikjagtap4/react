220982672009eb3ce9d09905a9bc4248c0107332
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-25 14:21:08 +0530

    basics of react

--- coderReact/src/App.jsx
+++ coderReact/src/App.jsx
@@ -3,13 +3,24 @@ import Video from "./component/Video"
 import Data from './data'
 import Play from './component/Play'
 import Counter from './component/Counter'
+import { useState } from 'react'
 function App() {
-  console.log("render App")
+  const [videos, setVideo] = useState(Data.slice(0,3))
+  function addVideo(){
+    for(let i = videos.length; i< Data.length   ;i++)
+    {
+      setVideo([...videos , Data[videos.length] ])
+    }
+  }
+
   return (
     <>
+    <div>
+      <button onClick={addVideo}>Add Video</button>
+    </div>
       <div className='component'>
         {
-          Data.map((video) => {
+          videos.map((video) => {
             return <Video {...video} >
               <Play onPlay={() => console.log("Playing " + video.title)} onPause={() => console.log("paused " + video.title)}>{"play " + video.title} </Play>
             </Video>
--- coderReact/src/data.js
+++ coderReact/src/data.js
@@ -23,7 +23,40 @@ const Data = [
     link:"https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png",
     verified : false ,
     key :3
-  }];
+  },
+  {
+    title : "HTML Tutorial",
+    channel :"Code With Harry",
+    views : "100k",
+    link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1HVNHQmF6XqXS0xqpvfcJFY3cQIAQEB3XmJ_edOZdMQ&s",
+    verified : false ,
+    key :4
+  },
+  {
+    title : "JavaScript Tutorial",
+    channel :"Chai Aur Code",
+    views : "300k",
+    link:"https://logos-world.net/wp-content/uploads/2023/02/JavaScript-Symbol.png",
+    verified : false ,
+    key :5
+  },
+  {
+    title : "CSS Tutorial",
+    channel :"Apna College",
+    views : "70k",
+    link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHIpkmVksg02cC7e2AucNjn_FmK02LLFZjOHK5c4hZzA&s",
+    verified : false ,
+    key :6
+  },
+  {
+    title : "DSA Tutorial",
+    channel :"ChedoTech Institute",
+    views : "35k",
+    link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZfFIjJBgO_k3yOPYbpYs5XDQXMmxK8Z_LzmsZ8-sgw&s",
+    verified : false ,
+    key :7
+  }
+];
 
 
   export default Data;
