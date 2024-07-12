1b4446b5860993fdbddfffec4c8d507dbb3dc67d
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-12 11:00:03 +0530

    Password Generator

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import './App.css'
 
@@ -9,6 +9,8 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
+  const passref = useRef(null);
+
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -16,7 +18,7 @@ function App() {
     if (numAllowed) str += "0123456789";
     if (charAllowed) str += "~!@#$%^&*-+?";
 
-    for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i < length; i++) {
       let index = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(index);
     }
@@ -24,27 +26,38 @@ function App() {
     setPassword(pass);
   }, [length, numAllowed, charAllowed])
 
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numAllowed, charAllowed])
 
+  const copyToClipBoard = useCallback(() => {
+    // console.log(password)
+    passref.current?.select();
+    passref.current?.setSelectionRange(0, 10)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
         <h1 className='text-white text-center my-3'>Password generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
           <input
             type="text"
             value={password}
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-
+            ref={passref}
           />
           <button
-
+            onClick={copyToClipBoard}
             className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
           >copy</button>
 
         </div>
+
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input
@@ -57,17 +70,19 @@ function App() {
             />
             <label>Length: {length}</label>
           </div>
+
           <div className="flex items-center gap-x-1">
             <input
               type="checkbox"
               defaultChecked={numAllowed}
               id="numberInput"
               onChange={() => {
-                setNumberAllowed((prev) => !prev);
+                setNumAllowed((prev) => !prev);
               }}
             />
             <label htmlFor="numberInput">Numbers</label>
           </div>
+
           <div className="flex items-center gap-x-1">
             <input
               type="checkbox"
