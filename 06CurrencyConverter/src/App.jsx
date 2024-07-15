70ed3e5fd9eec155fd54d8f9401f20992aed28ab
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-15 10:24:37 +0530

    currency converter

---
+++ 06CurrencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-orange-600 text-2xl'>Hello </h1>
+    </>
+  )
+}
+
+export default App
